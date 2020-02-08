ed9e72ddd0585b97ed254de0b448546778acc081
Monty Galloway <monty@humaan.com.au>
2020-02-08 11:50:32 +1100

    Add Grid Components

---
+++ src/layout/Grid.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export function Grid({ children }) {
+    return <div className="grid gutter">{children}</div>;
+}
+
+export function GridItem({
+    url,
+    image,
+    title,
+}) {
+    return (
+        <div className="grid-item">
+            <a href="#">
+                <div className="grid-item--image">
+                    <img src={image} />
+                </div>
+                <div className="grid-item--title">{title}</div>
+            </a>
+        </div>
+    )
+}
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,16 +1,26 @@
 import React from 'react';
-
+import { Grid, GridItem } from '../layout/Grid';
 import Page from '../layout/Page';
 
 export default function Home() {
-
+    const items = [
+        {
+            url: 'series',
+            title: 'Popular Series',
+            image: '/img/placeholder.png',
+        },
+        {
+            url: 'movies',
+            title: 'Popular Movies',
+            image: '/img/placeholder.png',
+        },
+    ];
 
     return (
         <Page title={'Popular Titles'}>
-            <div className="grid">
-                <div className="item">Series</div>
-                <div className="item">Movies</div>
-            </div>
+            <Grid>
+                {items.map((item, i) => <GridItem key={`grid-item-${i}`} {...item} />)}
+            </Grid>
         </Page>
     );
 }
