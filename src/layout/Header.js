b36cbc75f97f5d8f1dd2b0fd55e0230d1a70cb0e
Monty Galloway <monty@humaan.com.au>
2020-02-08 22:04:31 +1100

    Styles

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -4,14 +4,14 @@ import React from 'react';
 export default function Header() {
     return (
         <header className="header-main gutter">
-            <h1 className="header-main__logo">DEMO Streaming</h1>
-            <nav>
+            <h1 className="header-main--logo">DEMO Streaming</h1>
+            <nav className="header-main--nav">
                 <ul>
                     <li>
                         <a href="#">Log in</a>
                     </li>
                     <li>
-                        <a href="#">Start your free trial</a>
+                        <a href="#" className="free-trial-cta">Start your free trial</a>
                     </li>
                 </ul>
             </nav>
